2c7a07e5bf0f69a1c7a6a567bc48479f5dc209e1
brettnguyen <77566307+brettnguyen@users.noreply.github.com>
2023-02-28 18:41:13 -0500

    change

--- Server/models/db_connect.js
+++ Server/models/db_connect.js
@@ -1,14 +1,36 @@
 //require('dotenv').config();
 const dbConfig = require("../config/db.config.js");
 const mysql = require('mysql2');
-var connection = mysql.createPool({
+var con = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
+const query = (sql, binding) => {
+  return new Promise((resolve, reject) => {
+    con.query(sql, binding, (err, result, fields) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
 
-module.exports = connection;
+const createQuery = "CREATE DATABASE IF NOT EXISTS leaf_db;";
+con.query(createQuery);
+
+
+
+
+
+
+
+con.connect(function(err) {
+if (err) throw err;
+console.log('Database is connected successfully !');
+});
+
+module.exports = {con, query}
 /*
 const con = mysql.createConnection({
     host: process.env.MYSQL_HOST,
